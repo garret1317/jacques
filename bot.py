23040b55ec59d0e049adb2f08f9108d21a960766
garret <garret@airmail.cc>
2021-12-06 17:53:22 +0000

    do config with .env

--- bot.py
+++ bot.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
 import re
 
 import nextcord
+from dotenv import load_dotenv
 
 client = nextcord.Client()
 
-prefix = "?"
-baguette = ":french_bread:"
+load_dotenv()
+prefix = os.getenv("JACQUES_PREFIX")
+baguette = os.getenv("JACQUES_BAGUETTE")
+help_msg = os.getenv("JACQUES_HELP")
 
 
 @client.event
@@ -22,10 +26,10 @@ async def on_message(message):
         if command == "baguette":
             await message.channel.send(baguette)
         if command == "help":
-            await message.channel.send("no")
+            await message.channel.send(help_msg)
         if re.match("baguett+es", command):
             msg = baguette * command.count("t")
             await message.channel.send(msg)
 
 
-client.run("token")
+client.run(os.getenv("JACQUES_TOKEN"))
